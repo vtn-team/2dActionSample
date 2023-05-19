b578a80c77fbf6c0c9738735bf426a7bec83d5c9
k.mitarai <kazmit299@gmail.com>
2023-05-19 23:51:30 +0900

    弾の処理を変更

---
+++ Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    Player _player = null;
+
+    private void Start()
+    {
+        _player = GameObject.FindObjectOfType<Player>();
+    }
+
+    void Update()
+    {
+        this.transform.position += Vector3.left * 5 * Time.deltaTime;
+
+        if (Mathf.Abs(this.transform.position.x - _player.transform.transform.position.x) < (_player.transform.localScale.x + this.transform.localScale.x) / 2 &&
+            Mathf.Abs(this.transform.position.y - _player.transform.transform.position.y) < (_player.transform.localScale.y + this.transform.localScale.y) / 2)
+        {
+            _player.Damage();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    Vector3 _power = Vector3.right;
+
+    public void SetVector(Vector3 vec)
+    {
+        _power = vec;
+    }
+
+    void Update()
+    {
+        this.transform.position += _power * 5 * Time.deltaTime;
+
+        var enList = GameObject.FindObjectsOfType<Enemy>();
+        foreach (var enemy in enList)
+        {
+            if (Mathf.Abs(this.transform.position.x - enemy.transform.transform.position.x) < (enemy.transform.localScale.x + this.transform.localScale.x) / 2 &&
+                Mathf.Abs(this.transform.position.y - enemy.transform.transform.position.y) < (enemy.transform.localScale.y + this.transform.localScale.y) / 2)
+            {
+                enemy.Damage();
+                Destroy(gameObject);
+            }
+        }
+    }
+}
