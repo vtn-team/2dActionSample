435d7114d5350994097e7dcba519c9d9e92cd096
k.mitarai <kazmit299@gmail.com>
2023-05-22 00:13:07 +0900

    計算式を公式ベースに汎用的に修正

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,10 +6,11 @@ public class Player : Character
 {
     [SerializeField] float _speed = 5.0f;
     [SerializeField] float _extinc = 0.3f;
+    [SerializeField] float _gvScale = 0.3f;
     [SerializeField] float _scale = 10.0f;
     [SerializeField] float _jumpPow = 10.0f;
     float _jumpY = 0.0f;
-    float _grav = 0.0f;
+    float _jtime = 0.0f;
 
     private void Start()
     {
@@ -31,20 +32,27 @@ public class Player : Character
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                _jumpY = _jumpPow;
-                _grav = 0.0f;
+                _jumpY = _jumpPow; //初速
+                _jtime = 0.0f;
             }
         }
         else
         {
+            /*
             _grav += Time.deltaTime;
             _jumpY *= 1.0f - Time.deltaTime * _extinc;
             float y = this.transform.position.y;
             y += (_jumpY - 9.81f * _grav) * Time.deltaTime * _scale;
+            */
+            _jtime += Time.deltaTime; //経過時間
+            _jumpY *= 1.0f - Time.deltaTime * _extinc; //力の減衰
+            float y = this.transform.position.y;
+            y = (_jumpY*_jtime - 9.81f * _jtime * _jtime * _gvScale) * _scale + -4.0f; //(力*時間+重力加速度*重力の強さ) * スケール
+
             if(y < -4.0f)
             {
                 y = -4;
-                _grav = 0;
+                _jtime = 0;
                 _jumpY = 0;
             }
             this.transform.position = new Vector3(this.transform.position.x, y, this.transform.position.z);
